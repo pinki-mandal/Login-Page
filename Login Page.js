1178d85bd09ebe0a58dc3ed85dfb7a73325fe27f
pinki-mandal <99741856+pinki-mandal@users.noreply.github.com>
2023-07-25 20:49:37 +0530

    Add files via upload

---
+++ Login Page.js
@@ -0,0 +1,52 @@
+
+
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform login logic here, e.g., validate credentials
+    if (email === 'example@example.com' && password === 'password') {
+      setIsLoggedIn(true);
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  if (isLoggedIn) {
+    return <h2 className="welcome-message">Welcome, {email}!</h2>;
+  }
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
